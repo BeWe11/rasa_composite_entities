be0a3db4a41da2b44d78cef26265ea48e5991a04
Benjamin Weigang <Benjamin.Weigang@mailbox.org>
2019-01-17 13:28:56 +0100

    Add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rasa_composite_entities",
+    version="0.0.1",
+    author="Benjamin Weigang",
+    author_email="Benjamin.Weigang@mailbox.org",
+    description="A Rasa NLU component for composite entities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BeWe11/rasa_composite_entities",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
