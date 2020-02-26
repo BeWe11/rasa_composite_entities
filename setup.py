6b8e3a3c5984568aa10f6c1372d36ce55a9c5278
Benjamin Weigang <Benjamin.Weigang@mailbox.org>
2020-02-26 10:56:22 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rasa_composite_entities",
-    version="1.0.0",
+    version="1.0.1",
     author="Benjamin Weigang",
     author_email="Benjamin.Weigang@mailbox.org",
     description="A Rasa NLU component for composite entities",
